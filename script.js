6873507c530f9b48f7b635d780c3fd3781868ec0
biafarrugia <71879879+biafarrugia@users.noreply.github.com>
2020-12-06 05:17:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ScrollReveal().reveal("div", {delay: 100});
 ScrollReveal().reveal("p", { delay: 150, duration: 1100 });
+ScrollReveal().reveal("header", {delay: 50});
 ScrollReveal().reveal("h1", {delay: 100});
 ScrollReveal().reveal("h2", {delay: 50});
-
 ScrollReveal().reveal("figure", { origin: "right", distance: "1000px", duration: 1500});
