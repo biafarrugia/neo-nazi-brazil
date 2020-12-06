4e4bddd47fc93228e1795d4c7f2c54b3d234d571
biafarrugia <71879879+biafarrugia@users.noreply.github.com>
2020-12-06 06:40:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ ScrollReveal().reveal("h2", {delay: 50});
 ScrollReveal().reveal("figure", { origin: "right", distance: "1000px", duration: 1500});
 ScrollReveal().reveal("twitter-tweet", {delay: 50});
 ScrollReveal().reveal("content-subhead", { delay: 150, duration: 1100 });
+ScrollReveal().reveal("blockquote", { delay: 150, duration: 1100 });
